237bb47818d767b9adcd633290d54fccb5fc8212
GlnVDu <egor.deynega@bk.ru>
2023-11-16 03:43:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int i, j, b, n, vibor, f;
+    int max = 1000;
+    int min = -1000;
+    int a[5];
+    cout << "Введите элементы массива: ";
+
+}
